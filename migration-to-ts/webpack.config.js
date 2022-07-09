7818bc821adc4924995b694b5e9261d8c3132c1a
thelastandrew <thelastandrew@gmail.com>
2022-07-09 13:09:51 +0300

    refactor: optimise mode request func

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -5,46 +5,45 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-  entry: path.resolve(__dirname, './src/index'),
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      { test: /\.ts$/i, use: 'ts-loader' },
-      {
-        test: /\.jpg$/i,
-        type: 'asset/resource',
-      },
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.jpg$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'img/[name][ext]',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new ESLingPlugin({ extensions: 'ts' }),
     ],
-  },
-  resolve: {
-    extensions: ['.ts'],
-  },
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'img/[name][ext]',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './src/index.html'),
-      filename: 'index.html',
-    }),
-    new CleanWebpackPlugin(),
-    new ESLingPlugin({ extensions: 'ts' }),
-  ],
 };
 
 module.exports = ({ mode }) => {
-  const isProductionMode = mode === 'prod';
-  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+    const envConfig = require(`./webpack.${mode}.config`);
 
-  return merge(baseConfig, envConfig);
+    return merge(baseConfig, envConfig);
 };
